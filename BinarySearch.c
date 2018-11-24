ddc084b9e13ece5891d614299ea0acc6bdb7c523
Leixiujie <982463478@qq.com>
2018-11-24 10:49:36 +0800

    Add files via upload

---
+++ BinarySearch.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int BinarySearch(int ST[10],int key){
+	int value;
+	int low,high,mid;
+	low=0;
+	high=10;
+	for(int i=0;low<high;i++){
+		mid=(int)(low+high)/2;
+		if()
+	}
+	return value;
+}
+
+void main(){
+	int ST[10];
+
+}
