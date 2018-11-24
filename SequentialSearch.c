0e17bcf6fae8d02a7fe045d3ff9671edccd1f5d5
Leixiujie <982463478@qq.com>
2018-11-24 11:32:48 +0800

    Update SequentialSearch.c

--- SequentialSearch.c
+++ SequentialSearch.c
@@ -3,10 +3,17 @@
 
 int SequentialSearch(int ST,int key){
 	for(int i=0;i<10;i++){
-		ST[I]
+		if(ST[i]==key) return i;
 	}
+	return -1;
 }
 
 int main(){
+	int ST[10];
+	for(int i=0;i<10;i++){
+		printf("Please input No.%d number:",i+1;)
+	}
 
+	printf("Please input the number what you want");
+	SequentialSearch(ST,);
 }
