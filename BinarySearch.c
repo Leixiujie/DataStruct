2c4e5b76ae54d0c753c2e49793ef0e792bf37ffa
Leixiujie <982463478@qq.com>
2018-11-28 18:12:30 +0800

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -2,25 +2,25 @@
 #include<stdlib.h>
 
 int BinarySearch(int ST[10],int key){
-	int value;
+	int value,i;
 	int low,high,mid;
 	low=0;
 	high=10;
-	for(int i=0;low<high;i++){
+	for(i=0;low<high;i++){
 		mid=low+high/2;
 		if(ST[mid]<key) low=mid+1;
 		else if(ST[mid]>key) high=mid-1;
-		else (return mid);
+		else return mid;
 	}
 	return -1;
 }
 
 void main(){
 	int ST[10];
-	int snum,index;
+	int i,snum,index;
 	
 	printf("Input numbers in order:\n");
-	for(int i=0;i<10;i++){
+	for(i=0;i<10;i++){
 		printf("Please input No.%d number:",i+1);
 		scanf("%d",&ST[i]);
 	}
